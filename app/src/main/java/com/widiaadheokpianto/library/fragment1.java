355b2f47d4c286862300f20417ab7509d6287644
widiaadheokpianto <widiaadheokpianto@gmail.com>
2018-03-24 22:56:16 +0700

    changes

--- app/src/main/java/com/widiaadheokpianto/library/fragment1.java
+++ app/src/main/java/com/widiaadheokpianto/library/fragment1.java
@@ -5,13 +5,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 
 public class fragment1 extends Fragment
 {
+    private WebView mWebView;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view_frag1 = inflater.inflate(R.layout.fragment1, container, false);
+
+        mWebView = view_frag1.findViewById(R.id.webview);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://www.google.com");
+
         return view_frag1;
     }
 }
